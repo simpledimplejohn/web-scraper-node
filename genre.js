3941feb976eda385210718cd4029503f5409f44e
John Blalock <simpledimplejohn@gmail.com>
2023-07-17 20:59:41 -0400

    updated genre scraper

--- genre.js
+++ genre.js
@@ -1,25 +1,20 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 
-const url="https://books.toscrape.com/catalogue/category/books_1/index.html";
+const url="https://books.toscrape.com/catalogue/category/books/mystery_3/index.html";
 
 async function getGenre(url){
     try{
         const response = await axios.get(url);
         const $ = cheerio.load(response.data);
-        const genre = [];
-        $('.nav-list li a').each((i,element)=>{
-            genre.push({
-                title: $(element).text(),
-                url: $(element).attr('href')
-            })
-        })
-        return genre;
+        const genre = $("h1").text();
+
+        console.log(genre);
     }catch(error){
         console.error(error);
     }
 }
 
-module.exports = getGenre;
+getGenre(url);
 
 // Path: index.js
